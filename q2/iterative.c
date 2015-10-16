dd0c7d879735371213d5bca24019724fea203e49
senchih <a2215689@yahoo.com.tw>
2015-10-16 18:20:03 +0800

    Implement q2 iterative and recursive

--- q2/iterative.c
+++ q2/iterative.c
@@ -1,6 +1,25 @@
-/* FIXME: Implement! */
+#include <stdio.h>
 
-int main()
+char smallest_character(char str[],char c)
 {
+    int i = 0;
+    while(str[i]!='\0') {
+        if(str[i]>c) {
+            return str[i];
+        }
+        i++;
+    }
+    return str[0];
+}
+int main(int argc,char *argv[])
+{
+    char array[]= {'a','c','e','k','s','t','u'};
+    char c='t';
+    char result_c;
+    result_c = smallest_character(array,c);
+    printf("%c\n",result_c);
+
     return 0;
 }
+
+
--- q2/recursive.c
+++ q2/recursive.c
@@ -1,6 +1,32 @@
-/* FIXME: Implement! */
+#include <stdio.h>
 
-int main()
+char smallest_character(char str[],char c)
 {
+    if(str[0]=='\0') {
+        return '\0';
+    } else if(str[0]>c) {
+        return str[0];
+    } else {
+        char x=smallest_character(++str,c);
+        str--;
+        if(x=='\0')
+            return str[0];
+        else if(x>c)
+            return x;
+        else
+            return str[0];
+    }
+}
+
+int main(int argc,char *argv[])
+{
+    char array[]= {'a','c','e','k','s','t','u'};
+    char c='t';
+    char result_c;
+    result_c = smallest_character(array,c);
+    printf("%c\n",result_c);
+
     return 0;
 }
+
+
