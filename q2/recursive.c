4c8bc19644a984925cac75025c606e693135b857
senchih <a2215689@yahoo.com.tw>
2015-10-17 21:50:03 +0800

    Debug '\0' to 127
    The end of char array is not \0 but 127,
    thanks to arnochiu for this information.

--- q2/recursive.c
+++ q2/recursive.c
@@ -1,30 +1,39 @@
 #include <stdio.h>
+#include <time.h>
 
 char smallest_character(char str[],char c)
 {
-    if(str[0]=='\0') {
-        return '\0';
+    if(str[0]==127) {
+        return 127;
     } else if(str[0]>c) {
         return str[0];
     } else {
         char x=smallest_character(++str,c);
         str--;
-        if(x=='\0')
+        if(x==127){
             return str[0];
+	}
         else if(x>c)
             return x;
-        else
+        else{
             return str[0];
+	}
     }
 }
 
 int main(int argc,char *argv[])
 {
-    char array[]= {'a','c','e','k','s','t','u'};
-    char c='t';
+    char array[]= {'a','c','e','d','e','f','g','h','k','m','o','q','s','t','w','x'};
+    char c='y';
     char result_c;
+    clock_t start,end;
+
+    start = clock();
     result_c = smallest_character(array,c);
+    end = clock();
+
     printf("%c\n",result_c);
+    printf("%lf\n", (end-start)/(double)(CLOCKS_PER_SEC));
 
     return 0;
 }
