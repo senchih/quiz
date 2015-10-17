c61eb644932ff69eb96684e65867717658c1fb17
senchih <a2215689@yahoo.com.tw>
2015-10-17 23:37:21 +0800

    homework as q4

--- q4/iterative.c
+++ q4/iterative.c
@@ -1,6 +1,28 @@
-/* FIXME: Implement! */
+#include <stdio.h>
+
+int maxSubArray(int A[], int n)
+{
+    int max = A[0];
+    int tmp_max = 0;
+    for (int i = 0; i < n; ++i) {
+        if (tmp_max < 0)
+            tmp_max = A[i];
+        else
+            tmp_max += A[i];
+
+        //choose the bigger
+        if( tmp_max > max )
+            max = tmp_max;
+    }
+    return max;
+}
 
 int main()
 {
+    int A[]= {-1, 2, 3, -4, 8 };
+    int n = sizeof(A) / sizeof(int);
+    int max = maxSubArray(A, n);
+    printf("%d\n", max);
+
     return 0;
 }
--- q4/recursive.c
+++ q4/recursive.c
@@ -1,6 +1,33 @@
-/* FIXME: Implement! */
+#include <stdio.h>
+
+
+int maxSubArray(int A[], int n)
+{
+	int sum = A[0];
+	int tmp_sum = A[0];
+	int tmp = 0;
+
+	if (n > 1) {
+		tmp_sum = maxSubArray(A+1, n-1);
+	}
+
+	for (int index = 0; index < n; ++index) {
+		tmp += A[index];
+		if (tmp > sum) sum = tmp;
+	}
+	if (sum > tmp_sum) 
+		return sum;
+	else 
+		return tmp_sum;
+}
+
 
 int main()
 {
+    int A[]= {-6, 9, -3, -4, 12, -5 };
+    int n = sizeof(A) / sizeof(int);
+    int max = maxSubArray(A, n);
+    printf("%d\n", max);
+
     return 0;
 }
